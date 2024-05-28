2ce4caccf36aaada5a379a5e50049f0bb5e6a351
kangwook1 <rkddnr2078@naver.com>
2024-05-28 13:53:31 +0900

    [Refactor] 테스트 삭제

--- src/test/java/com/appcenter/practice/PracticeApplicationTests.java
+++ src/test/java/com/appcenter/practice/PracticeApplicationTests.java
@@ -1,9 +1,8 @@
 package com.appcenter.practice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class PracticeApplicationTests {
 
 	@Test
